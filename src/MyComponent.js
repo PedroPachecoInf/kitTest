3b24184d012068f0e0d7bcf1e592751824b31748
Pedro Pacheco <pedrodanielpacheco96@hotmail.com>
2020-01-28 14:43:36 +0000

    Addded component

---
+++ src/MyComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MyComponent = () => {
+  const labeText = 'Enter name: ';
+  const buttonText = { text: 'Click here bro!' }
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">{labeText}</label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText.text}</button>
+    </div>
+  )
+};
+
+export default MyComponent;
